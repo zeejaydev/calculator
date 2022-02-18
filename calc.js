154140ca64fc8e277fcbffecdfcfc126835d2413
Zaid Jamil <zaidjamil@Zaids-MacBook-Pro.local>
2022-02-18 12:39:18 -0700

    Initial commit

---
+++ calc.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+	// This line closes the connection to the command line interface.
+	// reader.close()
+
+});
